53576fe72b603dc11421b36c270462a5067a39d5
Wes Walker <weswalker125@gmail.com>
2017-10-29 21:22:09 -0400

    remove packages

--- setup.sh
+++ setup.sh
@@ -15,8 +15,33 @@ mkdir -p $INSTALL_DIR
 chown -R $DEFAULT_USER:$DEFAULT_USER $INSTALL_DIR
 
 echo "Package updates and installations."
+# Packagages to remove
+pkgs="$pkgs
+idle python3-pygame python-pygame python-tk
+idle3 python3-tk
+python3-rpi.gpio
+python-serial python3-serial
+python-picamera python3-picamera
+python3-pygame python-pygame python-tk
+python3-tk
+debian-reference-en dillo x2x
+scratch nuscratch
+timidity
+smartsim penguinspuzzle
+pistore
+sonic-pi
+python3-numpy
+python3-pifacecommon python3-pifacedigitalio python3-pifacedigital-scratch-handler python-pifacecommon python-pifacedigitalio
+minecraft-pi python-minecraftpi
+wolfram-engine
+"
+for pkg in $pkgs; do
+	apt-get --assume-yes remove --purge $pkg
+done
+apt-get -y autoremove
 apt-get --assume-yes update
 apt-get --assume-yes install zip clamav logwatch
+
 
 # Turn off SSH by non-pem
 echo "Update SSH rules."
