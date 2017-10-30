0a55369834153e3282d8274587d45f0d854da41a
Wes Walker <weswalker125@gmail.com>
2017-10-29 20:17:20 -0400

    packaged, setup script.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+INSTALL_DIR=$HOME/apps
+DEFAULT_USER=pi
+
+mkdir -p $INSTALL_DIR
+
 sudo apt-get update
-sudo apt-get install maven
+sudo apt-get install openjdk-8-jre zip clamav logwatch
 
-cd floreantpos-code
+# Turn off SSH by non-pem
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+sudo sed -i -e 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
 
-mvn clean install
+unzip $BASE_DIR/floreant-raspbian-demo.zip -d $INSTALL_DIR
+chown $DEFAULT_USER:$DEFAULT_USER $INSTALL_DIR/floreantpos
+
+sudo su - $DEFAULT_USER
+
+cd $INSTALL_DIR/floreantpos
+
+JAVA_OPTS="-Xms64m -Xmx512m"
 
 # Run Floreant POS
-java -jar target/floreantpos-bin/floreantpos/floreantpos.jar 
+java $JAVA_OPTS -jar ./floreantpos.jar 
