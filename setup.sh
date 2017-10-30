4798d2562850fb07db8ecfd308e4ef7bc3cbf617
Wes Walker <weswalker125@gmail.com>
2017-10-29 20:44:43 -0400

    update script

--- setup.sh
+++ setup.sh
@@ -10,19 +10,24 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 INSTALL_DIR=$HOME/apps
 DEFAULT_USER=pi
 
+echo "Making install directory: $INSTALL_DIR ."
 mkdir -p $INSTALL_DIR
-chown -R $DEFAULT_USER:$DEFAULT_USER
+chown -R $DEFAULT_USER:$DEFAULT_USER $INSTALL_DIR
 
+echo "Package updates and installations."
 #apt-get --assume-yes update
-#apt-get --assume-yes install openjdk-8-jre zip clamav logwatch
+#apt-get --assume-yes install zip clamav logwatch
 
 # Turn off SSH by non-pem
+echo "Update SSH rules."
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 sed -i -e 's/UsePAM yes/UsePAM no/g' /etc/ssh/sshd_config
 
+echo "Unpack FloreantPOS."
 unzip $BASE_DIR/floreant-raspbian-demo.zip -d $INSTALL_DIR
 sudo chown $DEFAULT_USER:$DEFAULT_USER $INSTALL_DIR/floreantpos
 
+echo "Switching users: $DEFAULT_USER ."
 su - $DEFAULT_USER
 
 cd $INSTALL_DIR/floreantpos
@@ -30,4 +35,5 @@ cd $INSTALL_DIR/floreantpos
 JAVA_OPTS="-Xms64m -Xmx512m"
 
 # Run Floreant POS
-java $JAVA_OPTS -jar ./floreantpos.jar 
+echo "Running FloreantPOS."
+/usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/jre/bin/java $JAVA_OPTS -jar ./floreantpos.jar 
